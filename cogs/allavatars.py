e7d566702f66ff9fabcbc101c3b41e2be1ff244d
vanillyn <vanillyn@multiryzz.ch>
2025-12-09 14:40:34 -0500

    remove unused imports i thought i was using and format

--- cogs/allavatars.py
+++ cogs/allavatars.py
@@ -3,9 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 import aiohttp
 import zipfile
-import os
 from pathlib import Path
-import asyncio
 from PIL import Image
 import io
 
